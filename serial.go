b0df660f308d95e0ade1384ec67cd7376d7af2e9
gangstaJS <oz@ex.ua>
2017-11-02 23:16:01 +0200

    fix

--- serial.go
+++ serial.go
@@ -3,20 +3,71 @@ package main
 import (
 	"fmt"
 	//"log"
+	//"io"
 	//"github.com/jacobsa/go-serial/serial"
 	//"encoding/json"
 
 	"syscall"
 	"os"
 	"unsafe"
-
-	"bufio"
-	"io"
 )
 
 const (
 	TCSETS = 0x5402
 )
+
+//
+//func readSerial(h *Hub, p *string) {
+//	options := serial.OpenOptions{
+//		PortName:              *p,
+//		BaudRate:              115200,
+//		DataBits:              8,
+//		StopBits:              2,
+//		InterCharacterTimeout: 1000,
+//		//MinimumReadSize: 100,
+//	}
+//
+//	port, err := serial.Open(options)
+//
+//	defer port.Close()
+//
+//	if err != nil {
+//		log.Fatalf("serial.Open: %v", err)
+//	}
+//
+//	fmt.Println("Started read from: ", *p)
+//
+//	for {
+//		buf := make([]byte, 128)
+//
+//		n, err := port.Read(buf)
+//
+//		if err != nil {
+//
+//			if err != io.EOF {
+//				fmt.Println("Error reading from serial port: ", err)
+//			}
+//
+//		} else {
+//
+//			buf = buf[:n]
+//			str := fmt.Sprintf("%s", buf)
+//
+//			var dat map[string]interface{}
+//
+//			if err := json.Unmarshal(buf, &dat); err != nil {
+//				panic(err)
+//			}
+//
+//			setItem(dat)
+//
+//			fmt.Println("[Recived data]: ", str)
+//
+//			h.broadcast <- buf
+//
+//		}
+//	}
+//}
 
 func r(h *Hub, p *string) {
 	s, _ := os.OpenFile(*p, syscall.O_RDWR|syscall.O_NOCTTY|syscall.O_NONBLOCK, 0666)
@@ -42,27 +93,27 @@ func r(h *Hub, p *string) {
 		0,
 		0)
 
-	reader := bufio.NewReader(s)
-
 	for {
-		n, _, err := reader.ReadLine()
+		buf := make([]byte, 128)
+		n, err := s.Read(buf)
 
-		if err == io.EOF {
-			// fmt.Println(err)
+		if err != nil { // err will equal io.EOF
+			//fmt.Println(err)
 		} else {
-			str := fmt.Sprintf("%s", n)
+			buf = buf[:n]
+			str := fmt.Sprintf("%s", buf)
 
 			//var dat map[string]interface{}
 			//
-			//if err := json.Unmarshal(n, &dat); err != nil {
+			//if err := json.Unmarshal(buf, &dat); err != nil {
 			//	panic(err)
 			//}
-			//
+
 			//setItem(dat)
 
 			fmt.Println("[Recived data]: ", str)
 
-			h.broadcast <- []byte(n)
+			h.broadcast <- buf
 		}
 
 	}
